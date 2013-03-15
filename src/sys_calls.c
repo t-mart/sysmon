855dd29f00ae119d96dbc7cb41a03bd6dec26443
eric <ehuang3@gatech.edu>
2013-03-15 00:02:05 -0400

    fix sys_calls

--- src/sys_calls.c
+++ src/sys_calls.c
@@ -8,13 +8,13 @@ static char num_calls[] = {
 #include <asm/unistd.h>
 };
 
-const uint32_t __NR_syscall_max = sizeof(num_calls)-1;
+const uint32_t syscall_max = sizeof(num_calls)-1;
 
 #undef __syscall
 #undef _asm_x86_64_unistd_h
 #define __syscall(nr, sym) [ nr ] = { .sym_name = #sym, .sys_num = nr }, 
 
-const struct sys_call_id_t sys_call_table[__NR_syscall_max+1] = {
-   [0 ... __NR_syscall_max] = { .sym_name = "ERROR: invalid sys call", .sys_num = -1 },
+const struct sys_call_id_t sys_call_table[syscall_max+1] = {
+   [0 ... syscall_max] = { .sym_name = "ERROR: invalid sys call", .sys_num = -1 },
 #include <asm/unistd_64.h>
 };
