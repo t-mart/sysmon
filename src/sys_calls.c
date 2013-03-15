e5f654ca2135d45c7c87ef4562869844aaac2942
eric <ehuang3@gatech.edu>
2013-03-15 01:58:42 -0400

    fix sys_calls

--- src/sys_calls.c
+++ src/sys_calls.c
@@ -1,23 +1,10 @@
 #include "sys_calls.h"
 
-
-// 
 // See arch/x86/kernel/asm-offsets_64.c
 //     arch/x86/kernel/sys_calls.c
 //     include/asm-x86/unistd_64.h
-#define DEFINE(sym, val) \
-        asm volatile("\n->" #sym " %0 " #val : : "i" (val))
-
 #define __NO_STUBS
 
-#undef __SYSCALL
-#define __SYSCALL(nr, sym) [nr] = 1
-#undef _ASM_X86_64_UNISTD_H_
-static char num_calls[] = {
-//#include "helper_unistd_64.h"
-};
-
-//DEFINE(__NR_syscall_max, sizeof(num_calls)-1);
 // Just hardcode it
 #define __NR_syscall_max 285
 
@@ -26,6 +13,6 @@ static char num_calls[] = {
 #undef _ASM_X86_64_UNISTD_H_
 
 const struct sys_call_id_t sys_call_table[__NR_syscall_max+1] = {
-   [0 ... __NR_syscall_max] = { .sym_name = "ERROR: invalid sys call", .sys_num = -1 },
-#include "helper_unistd_64.h"
+   [0 ... __NR_syscall_max] = { .sym_name = "ERROR: invalid sys call index", .sys_num = -1 },
+   #include <asm/unistd_64.h>
 };
