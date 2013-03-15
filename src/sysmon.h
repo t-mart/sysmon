4553ba09fede2f9465c648df5b03ca5d3756ac00
eric <ehuang3@gatech.edu>
2013-03-15 03:28:53 -0400

    no printk

--- src/sysmon.h
+++ src/sysmon.h
@@ -6,12 +6,16 @@
 #define MODULE_NAME "sysmon"
 #define LOG_ID "[" MODULE_NAME "] "
 
-#define INFO_PRINT(fmt,...) printk(KERN_INFO LOG_ID fmt, ##__VA_ARGS__)
-#define ERR_PRINT(fmt,...) printk(KERN_ERR LOG_ID fmt, ##__VA_ARGS__)
 
 #define SYSMON_DEBUG
 #ifdef SYSMON_DEBUG
 #define DEBUG_PRINT(fmt,...) printk(KERN_DEBUG LOG_ID fmt, ##__VA_ARGS__)
+#define INFO_PRINT(fmt,...) printk(KERN_INFO LOG_ID fmt, ##__VA_ARGS__)
+#define ERR_PRINT(fmt,...) printk(KERN_ERR LOG_ID fmt, ##__VA_ARGS__)
+#else
+#define DEBUG_PRINT(fmt,...)
+#define INFO_PRINT(fmt,...)
+#define ERR_PRINT(fmt,...)
 #endif
 
 #endif
