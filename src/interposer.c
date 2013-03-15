2d3f76e920d72986d3f79d800abe4ee19594070a
eric <ehuang3@gatech.edu>
2013-03-15 08:54:45 -0400

    Limit sys calls to monitor

--- src/interposer.c
+++ src/interposer.c
@@ -6,7 +6,7 @@
 #include "buffer.h"
 #include "sys_calls.h"
 
-static struct kprobe probe[SYSCALL_MAX+1];
+static struct kprobe probe[sys_call_monitor_size];
 
 /* pt_regs defined in include/asm-x86/ptrace.h
  *
@@ -16,12 +16,24 @@ static struct kprobe probe[SYSCALL_MAX+1];
 static int sysmon_intercept_before(struct kprobe *kp, struct pt_regs *regs)
 {
 	int ret = 0;
+	long nr = -1;
 	char *sys_call;
 
 	if (current->uid != uid)
 		return 0;
 
-	sys_call = sys_call_table[regs->rax].sym_name;
+	// filter out unmonitored sys calls
+	int i;
+	for(i=0; i < sys_call_monitor_size; ++i) {
+		if(sys_call_monitor[i] == regs->rax) {
+			nr = regs->rax;
+			break;
+		}
+	}
+	if(nr == -1)
+		return 0;
+
+	sys_call = sys_call_table[nr].sym_name;
 
 	INFO_PRINT(
 		/* sycall pid tid args.. */
@@ -29,7 +41,7 @@ static int sysmon_intercept_before(struct kprobe *kp, struct pt_regs *regs)
 		"nr: %lu, pid: %d, tgid: %d, uid: %d\n",
 		"args (%lu, %lu, %lu, %lu, %lu, %lu)\n",
 		sys_call,
-	 	regs->rax, current->pid, current->tgid, current->uid,
+	 	regs->rax, current->pid, currentnr>tgid, current->uid,
 		regs->rdi, regs->rsi, regs->rdx, regs->r10, regs->r8, regs->r9);
 
 	if (sysmon_buffer_write(regs) != -1)
@@ -44,16 +56,18 @@ static int sysmon_intercept_before(struct kprobe *kp, struct pt_regs *regs)
 int start_interposer(void)
 {
 	int i;
+	long nr;
 	INFO_PRINT("setting up interposer...\n");
-	for(i=0; i <= SYSCALL_MAX; ++i) {
-		if(sys_call_table[i].sys_num == -1)
+	for(i=0; i < sys_call_monitor_size; ++i) {
+		nr = sys_call_monitor[i];
+		if(sys_call_table[nr].sys_num == -1)
 			continue; // invalid probe
 
-		probe[i].symbol_name = sys_call_table[i].sym_name;
+		probe[i].symbol_name = sys_call_table[nr].sym_name;
 		probe[i].pre_handler = sysmon_intercept_before; /* called prior to function */
 
 		if (register_kprobe(&probe[i])) {
-			ERR_PRINT("register_kprobe failed on '%s'!\n", sys_call_table[i].sym_name);
+			ERR_PRINT("register_kprobe failed on '%s'!\n", sys_call_table[nr].sym_name);
 			return -EFAULT;
 		}
 	}
@@ -64,9 +78,11 @@ int start_interposer(void)
 void stop_interposer(void)
 {
 	int i;
+	long nr;
 	INFO_PRINT("tearing down interposer...\n");
-	for(i=0; i <= SYSCALL_MAX; ++i) {
-		if(sys_call_table[i].sys_num == -1)
+	for(i=0; i < sys_call_monitor_size; ++i) {
+		nr = sys_call_monitor[i];
+		if(sys_call_table[nr].sys_num == -1)
 			continue; // invalid probe
 		unregister_kprobe(&probe[i]);
 	}
--- src/sys_calls.h
+++ src/sys_calls.h
@@ -10,4 +10,8 @@ struct sys_call_id_t {
 
 extern const struct sys_call_id_t sys_call_table[];
 
+// Table of sys calls to monitor by sys number, e.g. __NR_mkdir
+extern const int sys_call_monitor_size;
+extern const long sys_call_monitor[];
+
 #endif //_SYS_CALLS_H
