8854f95ee246b72134eb320f2d48541a2acf0648
eric <ehuang3@gatech.edu>
2013-03-15 00:44:58 -0400

    fix

--- src/sys_calls.c
+++ src/sys_calls.c
@@ -8,23 +8,24 @@
 #define DEFINE(sym, val) \
         asm volatile("\n->" #sym " %0 " #val : : "i" (val))
 
-#define __NO_STUBS 1
-#undef __syscall
-#undef _asm_x86_64_unistd_h
-#define __syscall(nr, sym) [nr] = 1
+#define __NO_STUBS
+
+#undef __SYSCALL
+#define __SYSCALL(nr, sym) [nr] = 1
+#undef _ASM_X86_64_UNISTD_H_
 static char num_calls[] = {
-#include <asm/unistd.h>
+//#include "helper_unistd_64.h"
 };
 
 //DEFINE(__NR_syscall_max, sizeof(num_calls)-1);
 // Just hardcode it
 #define __NR_syscall_max 285
 
-#undef __syscall
-#undef _asm_x86_64_unistd_h
-#define __syscall(nr, sym) [ nr ] = { .sym_name = #sym, .sys_num = nr }, 
+#undef __SYSCALL
+#define __SYSCALL(nr, sym) [ nr ] = { .sym_name = #sym, .sys_num = nr }, 
+#undef _ASM_X86_64_UNISTD_H_
 
 const struct sys_call_id_t sys_call_table[__NR_syscall_max+1] = {
    [0 ... __NR_syscall_max] = { .sym_name = "ERROR: invalid sys call", .sys_num = -1 },
-#include <asm/unistd_64.h>
+#include "helper_unistd_64.h"
 };
